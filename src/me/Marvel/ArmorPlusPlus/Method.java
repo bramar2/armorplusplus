c5334f5e007552dfb6e5b81668d2509599d686c5
bramar2 <57794236+bramar2@users.noreply.github.com>
2020-08-07 11:50:30 +0700

    Method.java
    Static methods to help me

---
+++ src/me/Marvel/ArmorPlusPlus/Method.java
@@ -0,0 +1,78 @@
+package me.Marvel.ArmorPlusPlus;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.Plugin;
+
+public class Method {
+	/*
+	 * Automates the method of making a recipe and checking armor for abilities, 
+	 * so the code will be much simpler (i think & i hope)
+	 * 2020
+	 */
+	public static Plugin plugin;
+	public static boolean ifWearingAll(Player p, String armorname, String lore1) {
+		boolean result = false;
+		try {
+			ItemStack helm = p.getInventory().getHelmet();
+			ItemStack chest = p.getInventory().getChestplate();
+			ItemStack legs = p.getInventory().getLeggings();
+			ItemStack feet = p.getInventory().getBoots();
+			if(helm.getItemMeta().getDisplayName().equalsIgnoreCase(armorname + " Helmet") &&
+				chest.getItemMeta().getDisplayName().equalsIgnoreCase(armorname + " Chestplate") &&
+				legs.getItemMeta().getDisplayName().equalsIgnoreCase(armorname + " Leggings") && 
+				feet.getItemMeta().getDisplayName().equalsIgnoreCase(armorname + " Boots")) {
+				if(helm.getItemMeta().getLore().get(0).equalsIgnoreCase(lore1) && 
+					chest.getItemMeta().getLore().get(0).equalsIgnoreCase(lore1) &&
+					legs.getItemMeta().getLore().get(0).equalsIgnoreCase(lore1) && 
+					feet.getItemMeta().getLore().get(0).equalsIgnoreCase(lore1)) {
+					result = true;
+				}else {
+					result = false;
+				}
+			}else {
+				result = false;
+			}
+			return result;
+		}catch(Exception e) {
+			return false;
+		}
+	}
+	public static void changeArmorInArmorSlot(Player p, ItemStack helm, ItemStack chest, ItemStack legs, ItemStack feet) {
+		try {
+			p.getInventory().setHelmet(helm);
+			p.getInventory().setChestplate(chest);
+			p.getInventory().setLeggings(legs);
+			p.getInventory().setBoots(feet);
+		}catch(Exception e) {
+			
+		}
+	}
+	public static void replaceArmorInInventory(Player p, String tag, ItemStack[] newArmor) {
+		ItemStack[] content = p.getInventory().getContents();
+		for(int i = 0; i < content.length; i++) {
+			ItemStack item = content[i];
+			try {
+				if(item.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(plugin, tag + "Helm"), PersistentDataType.BYTE)) {
+					// helmet
+					content[i] = newArmor[0];
+				}else if(item.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(plugin, tag + "Chest"), PersistentDataType.BYTE)) {
+					// chestplate
+					content[i] = newArmor[1];
+				}else if(item.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(plugin, tag + "Legs"), PersistentDataType.BYTE)) {
+					// legs
+					content[i] = newArmor[2];
+				}else if(item.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(plugin, tag + "Feet"), PersistentDataType.BYTE)) {
+					// feet
+					content[i] = newArmor[3];
+				}
+			}catch(Exception e) {
+				
+			}
+			continue;
+		}
+		p.getInventory().setContents(content);
+	}
+}
