03fd6e561b01f272f576fd1b23314f323a08a112
bramar2 <57794236+bramar2@users.noreply.github.com>
2020-10-05 14:28:37 +0700

    Update ArmorPlusPlus.java

--- src/me/marvel/armorplusplus/ArmorPlusPlus.java
+++ src/me/marvel/armorplusplus/ArmorPlusPlus.java
@@ -1,30 +1,22 @@
 package me.marvel.armorplusplus;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import me.marvel.armorplusplus.commands.TabComplete;
 public class ArmorPlusPlus extends JavaPlugin implements Listener {
 	
-	/*
-	 * THIS PROGRAM USES JSOUP AS TO CHECK UPDATES.
-	 * CREDITS TO JSOUP: JSOUP.ORG
-	 */
-	
 	public boolean outdated = false;
 	public boolean uptodate = false;
 	public String msg = "";
+	public UpdateChecker uc;
 	
 	@EventHandler
 	public void onJoinEvent(PlayerJoinEvent e) {
-		if(e.getPlayer().isOp()) {
+		if(e.getPlayer().isOp() || e.getPlayer().hasPermission("armorplusplus.cheat")) {
 			if(outdated) {
 				if(msg != "") e.getPlayer().sendMessage(msg);
 				else e.getPlayer().sendMessage(ChatColor.GREEN + "ArmorPlusPlus > Available update version. Current plugin is outdated. Download at https://www.spigotmc.org/resources/armorplusplus.74748/");
@@ -35,57 +27,35 @@ public class ArmorPlusPlus extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
-		try {
-			if(getConfig().getBoolean("check-update")) {
-				try {
-					String url = "https://www.spigotmc.org/resources/armorplusplus.74748/";
-					String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36";
-					Document doc = Jsoup.connect(url).userAgent(userAgent).get();
-					Element resourceInfo = doc.select("div.resourceInfo").get(0);
-					String version = resourceInfo.select("h1 span.muted").get(0).text();
-					if(!version.equalsIgnoreCase(getDescription().getVersion())) {
-						boolean noteligible = false;
-						try {
-							int a = Integer.parseInt(getDescription().getVersion().replace(".", "").replace(" ", ""));
-							if(a == 0) {
-								// do nothing
-							}
-						}catch(Exception e) {
-							getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "ArmorPlusPlus > Detected invalid version. Please don't modify the plugin's version.");
-							noteligible = true;
-						}
-						if(noteligible) {
-							
-						}
-						else if(Integer.parseInt(version.replace(".", "").replace(" ", "")) < Integer.parseInt(getDescription().getVersion().replace(".", "").replace(" ", ""))) {
-							getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "ArmorPlusPlus > Detected invalid version. Please don't modify the plugin's version.");
-						}else {
-							// run something
-							outdated = true;
-							msg = ChatColor.GREEN + "ArmorPlusPlus > Available update version " + version + ". Current plugin has the version " + getDescription().getVersion() + ". Download at " + url;
-							getServer().getConsoleSender().sendMessage(msg);
-							for(Player p : getServer().getOnlinePlayers()) {
-								if(p.isOp()) {
-									p.sendMessage(msg);
-								}
-							}
-						}
-					}else {
-						uptodate = true;
-						getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "ArmorPlusPlus > No available updates. This version is up-to-date.");
-					}
-				}catch(Exception e) {
-					getServer().getConsoleSender().sendMessage(ChatColor.RED + "ArmorPlusPlus > Failed to check version updates.");
-				}
-			}
-		}catch(Exception e) {
-			
-		}
 		this.saveDefaultConfig();
 		this.getConfig().options().copyDefaults(true);
 		getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "==========================");
 		getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "Armor++ plugin enabled v" + this.getDescription().getVersion());
 		getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "==========================");
+		try {
+			if(getConfig().getBoolean("check-update")) {
+				uc = new UpdateChecker(this, 74748);
+				uc.getVersion((version) -> {
+					if(version == null) {
+						getServer().getConsoleSender().sendMessage(ChatColor.RED + "ArmorPlusPlus > Unable to check updates. To manually check updates, please type /armorplusplus check.");
+					}else {
+						if(version.equalsIgnoreCase(getDescription().getVersion())) {
+							outdated = false;
+							uptodate = true;
+							getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "ArmorPlusPlus > No available updates.");
+						}else {
+							outdated = true;
+							uptodate = false;
+							msg = ChatColor.GREEN + "ArmorPlusPlus > Available update version " + version + ". Download at https://www.spigotmc.org/resources/armorplusplus.74748/";
+							getServer().getConsoleSender().sendMessage(msg);
+						}
+					}
+				});
+			}
+		}catch(Exception e) {
+			getLogger().warning(ChatColor.RED + "ArmorPlusPlus > Unable to check updates. To manually check updates, please type /armorplusplus check.");
+		}
+		
 		getCommand("armorplusplus").setExecutor(new me.marvel.armorplusplus.commands.ArmorPlusPlus(this, this));
 		getCommand("armorplusplus").setTabCompleter(new TabComplete());
 		getServer().getPluginManager().registerEvents(new me.marvel.armorplusplus.commands.ArmorPlusPlus(this, this), this);
@@ -118,6 +88,7 @@ public class ArmorPlusPlus extends JavaPlugin implements Listener {
 		ar.quartzArmor();
 		ar.obsidianArmor();
 		ar.emeraldArmor();
+		ar.slimeArmor();
 	}
 	private void loadAbilities() {
 			// For every 2.5 second
